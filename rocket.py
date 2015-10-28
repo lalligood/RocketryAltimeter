04127dd84f9d5b3bb8f8261479dba82be1995f3f
Lance Alligood <lalligood@gmail.com>
2015-10-28 12:48:24 -0400

    OOP: +Refined Measurement.max/min/avg() +Wrapped main into main()
    +Tweaked parse_file() to be more flexible (in case I use other altimeters)

--- rocket.py
+++ rocket.py
@@ -17,16 +17,19 @@ class Measurement:
         self.title = title
 
     def max(self):
-        'Return maximum value in the list'
-        return max(int(meas) for meas in self.measure)
+        'Print maximum value in the list'
+        result = max(float(meas) for meas in self.measure)
+        print('Maximum {}: {}'.format(self.unit, result))
 
     def min(self):
-        'Return minimum value in the list'
-        return round(min(float(meas) for meas in self.measure), 2)
+        'Print minimum value in the list'
+        result = round(min(float(meas) for meas in self.measure), 2)
+        print('Minimum {}: {}'.format(self.unit, result))
 
     def avg(self):
-        'Return average value in the list'
-        return round(sum(float(meas) for meas in self.measure) / (len(self.measure)), 1)
+        'Print average value in the list'
+        result = round(sum(float(meas) for meas in self.measure) / (len(self.measure)), 1)
+        print('Average {}: {}'.format(self.unit, result))
 
 class ChartElement:
     def __init__(self, x, y, legend, plot_file, title):
@@ -78,7 +81,7 @@ Perfectflite files found:'''.format(os.getcwd()))
                     break # Break for loop
                 break # Break while loop
 
-def parse_file(flight_file):
+def parse_file(flight_file, height, seconds, speed, heat, power):
     'Open the file, read data in, & parse into arrays'
     open_file = open(flight_file, 'r')
     full_file = csv.reader(open_file)
@@ -89,41 +92,50 @@ def parse_file(flight_file):
             if match and len(row) == 5:
                 raw_data.append(row)
     flight_data = list(raw_data)
-    # Create 5 empty arrays
-    alti = Measurement('Altitude (ft)', 'Altitude Profile')
-    time = Measurement('Time (sec)', '')
-    velo = Measurement('Velocity (ft/sec)', 'Flight Velocity')
-    temp = Measurement('Temperature (degrees F)', 'Ambient Temperature')
-    volt = Measurement('Voltage (V)', 'Battery Voltage')
     for x in range(len(flight_data)):
-        time.measure.append(flight_data[x][0]) # For flight time readings
-        alti.measure.append(flight_data[x][1]) # For altimeter height readings
-        velo.measure.append(flight_data[x][2]) # For velocity readings
-        temp.measure.append(flight_data[x][3]) # For ambient temperature readings
-        volt.measure.append(flight_data[x][4]) # For battery voltage readings
+        seconds.measure.append(flight_data[x][0]) # For flight time readings
+        height.measure.append(flight_data[x][1]) # For altimeter height readings
+        speed.measure.append(flight_data[x][2]) # For velocity readings
+        heat.measure.append(flight_data[x][3]) # For ambient temperature readings
+        power.measure.append(flight_data[x][4]) # For battery voltage readings
     open_file.close()
-    return alti, time, velo, temp, volt
 
-# MAIN ROUTINE
-while True:
-    my_flight = select_flight()
-    alti, time, velo, temp, volt = parse_file(my_flight)
-    print('Maximum {}: {}'.format(alti.unit, alti.max()))
-    print('Maximum {}: {}'.format(velo.unit, velo.max()))
-    print('Average {}: {}'.format(temp.unit, temp.avg()))
-    print('Minimum {}: {}'.format(volt.unit, volt.min()))
+def main():
+    'Main routine for selecting file, parsing the data, & displaying charts.'
+    while True:
+        # Select flight
+        my_flight = select_flight()
 
-    # Plot altitude
-    altitude = ChartElement(time.measure, alti.measure, alti.unit, my_flight, alti.title)
+        # Initialize variables to hold measurements
+        alti = Measurement('Altitude (ft)', 'Altitude Profile')
+        time = Measurement('Time (sec)', '')
+        velo = Measurement('Velocity (ft/sec)', 'Flight Velocity')
+        temp = Measurement('Temperature (degrees F)', 'Ambient Temperature')
+        volt = Measurement('Voltage (V)', 'Battery Voltage')
 
-    # Plot velocity
-    velocity = ChartElement(time.measure, velo.measure, velo.unit, my_flight, velo.title)
+        # Parse file & populate variables
+        parse_file(my_flight, alti, time, velo, temp, volt)
 
-    # Plot temperature
-    temperature = ChartElement(time.measure, temp.measure, temp.unit, my_flight, temp.title)
+        # Display basic statistics about flight
+        alti.max()
+        velo.max()
+        temp.avg()
+        volt.min()
 
-    # Plot battery voltage
-    voltage = ChartElement(time.measure, volt.measure, volt.unit, my_flight, volt.title)
+        # Plot altitude
+        altitude = ChartElement(time.measure, alti.measure, alti.unit, my_flight, alti.title)
 
-    # Display plot windows
-    plt.show()
+        # Plot velocity
+        velocity = ChartElement(time.measure, velo.measure, velo.unit, my_flight, velo.title)
+
+        # Plot temperature
+        temperature = ChartElement(time.measure, temp.measure, temp.unit, my_flight, temp.title)
+
+        # Plot battery voltage
+        voltage = ChartElement(time.measure, volt.measure, volt.unit, my_flight, volt.title)
+
+        # Display plot windows
+        plt.show()
+
+if __name__ == "__main__":
+    main()
