05e5ba626355eb7c0f1dc7c9b2745bb058a16016
Lance Alligood <lalligood@gmail.com>
2015-12-19 21:40:38 -0500

    +Better handling of methods when column is empty
    +Resolved poor handling when less than 5 columns in row.

--- rocket.py
+++ rocket.py
@@ -18,18 +18,27 @@ class Measurement:
 
     def max(self):
         'Print maximum value in the list'
-        result = max(float(meas) for meas in self.measure)
-        print('Maximum {}: {}'.format(self.unit, result))
+        try:
+            result = max(float(meas) for meas in self.measure)
+            print('Maximum {}: {}'.format(self.unit, result))
+        except ValueError:
+            None
 
     def min(self):
         'Print minimum value in the list'
-        result = round(min(float(meas) for meas in self.measure), 2)
-        print('Minimum {}: {}'.format(self.unit, result))
+        try:
+            result = round(min(float(meas) for meas in self.measure), 2)
+            print('Minimum {}: {}'.format(self.unit, result))
+        except ValueError:
+            None
 
     def avg(self):
         'Print average value in the list'
-        result = round(sum(float(meas) for meas in self.measure) / (len(self.measure)), 1)
-        print('Average {}: {}'.format(self.unit, result))
+        try:
+            result = round(sum(float(meas) for meas in self.measure) / (len(self.measure)), 1)
+            print('Average {}: {}'.format(self.unit, result))
+        except ValueError:
+            None
 
 class ChartElement:
     def __init__(self, x, y, legend, plot_file, title):
@@ -60,15 +69,12 @@ Perfectflite files found:'''.format(os.getcwd()))
         for files in glob.glob('*.pf2'):
             count += 1
             print('\t{}. {}'.format(count, files))
-        print('\tX. Exit')
-        selection = input('Enter the number of the file you want to plot or exit: ')
-        if selection.lower() == 'x':
+        print('\t0. Exit')
+        selection = int(input('Enter the number of the file you want to plot or exit: '))
+        if selection == 0:
             print('Exiting Rocket Flight Plotter...')
             sys.exit(0)
-        elif not selection.isnumeric():
-            errmsgslow('Invalid selection. Please try again...')
-            continue
-        elif int(selection) < 1 or int(selection) > count:
+        elif selection < 0 or selection > count:
             errmsgslow('Invalid selection. Please try again...')
             continue
         else:
@@ -76,7 +82,7 @@ Perfectflite files found:'''.format(os.getcwd()))
             counter = 0
             for files in glob.glob('*.pf2'):
                 counter += 1
-                if counter == int(selection):
+                if counter == selection:
                     print('You selected {}'.format(flight_file))
                     return flight_file
                     break # Break for loop
@@ -88,18 +94,18 @@ def parse_file(input_file, col0, col1, col2='0', col3='0', col4='0'):
     full_file = csv.reader(open_file)
     raw_data = [] # Empty list to put each line of the file into
     for row in full_file:
+        row_len = len(row)
         for idx in row:
             match = re.search('^\d', idx) # Only want lines starting with digit
-            if match and len(row) > 2:
+            if match and row_len > 2:
                 raw_data.append(row)
-            row_len = len(row)
     flight_data = list(raw_data)
     for x in range(len(flight_data)):
         col0.measure.append(flight_data[x][0])
         col1.measure.append(flight_data[x][1])
-        if row_len == 3:
+        if row_len >= 3:
             col2.measure.append(flight_data[x][2])
-        if row_len == 4:
+        if row_len >= 4:
             col3.measure.append(flight_data[x][3])
         if row_len == 5:
             col4.measure.append(flight_data[x][4])
@@ -110,36 +116,28 @@ def main():
     while True:
         # Select flight
         my_flight = select_flight()
-
         # Initialize variables to hold measurements
-        time = Measurement('Time (sec)', '')
+        time = Measurement('Time (sec)', None)
         alti = Measurement('Altitude (ft)', 'Altitude Profile')
         velo = Measurement('Velocity (ft/sec)', 'Flight Velocity')
         temp = Measurement('Temperature (degrees F)', 'Ambient Temperature')
         volt = Measurement('Voltage (V)', 'Battery Voltage')
-
         # Parse file & populate variables
         parse_file(my_flight, time, alti, velo, temp, volt)
-
         # Display basic statistics about flight
         time.max()
         alti.max()
         velo.max()
         temp.avg()
         volt.min()
-
         # Plot altitude
         altitude = ChartElement(time.measure, alti.measure, alti.unit, my_flight, alti.title)
-
         # Plot velocity
         velocity = ChartElement(time.measure, velo.measure, velo.unit, my_flight, velo.title)
-
         # Plot temperature
         temperature = ChartElement(time.measure, temp.measure, temp.unit, my_flight, temp.title)
-
         # Plot battery voltage
         voltage = ChartElement(time.measure, volt.measure, volt.unit, my_flight, volt.title)
-
         # Display plot windows
         plt.show()
 
